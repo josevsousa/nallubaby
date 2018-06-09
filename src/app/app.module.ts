fde993d0effaea48bf3ef1b0343fb6653636ff2c
josevsousa <jose.vicente.de.sousa@gmail.com>
2018-06-09 14:55:50 -0300

    config material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MatButtonModule } from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
